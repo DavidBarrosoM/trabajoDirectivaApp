76fcc6710e135e3e6c1cb3459999d42fb95b4d45
A940731 <david.barroso.external@eviden.com>
2025-04-24 10:15:59 +0200

    trabajo con directivas ngIf y ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  tituloPagina = 'Registro de usuarios';
+  mensaje = '';
+  registrado = false;
+  nombre: string = '';
+  apellido: string = '';
+  entradas = [
+      {titulo:"Pythor cada día mas presente"},
+      {titulo:"Java presente desde hace más de 20 años"},
+      {titulo:"JavaScript cada vez más funcional"},
+      {titulo:"Kotlin potencia para tus apps"},
+      {titulo:"¿Dónde quedó pascal?"},
+  ];
+/* Esto no funciona
+  entradas: Object[];
+  constructor() {
+    this.entradas = [
+      {titulo:"Pythor cada día mas presente"},
+      {titulo:"Java presente desde hace más de 20 años"},
+      {titulo:"JavaScript cada vez más funcional"},
+      {titulo:"Kotlin potencia para tus apps"},
+      {titulo:"¿Dónde quedó pascal?"},
+    ];
+  }
+    */
+  registrarUsuario(){
+    this.registrado = true;
+    this.mensaje = 'Usuario registrado con éxito';
+  }
 }
